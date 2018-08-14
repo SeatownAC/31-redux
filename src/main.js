6bbf724ab8e283aa8ff24673daba1c6762f925d5
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-08-13 20:56:19 -0700

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './style/main.scss';
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './components/app.js';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
+ReactDom.render(<Main />, document.getElementById('root'));
